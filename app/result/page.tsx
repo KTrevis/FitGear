b786ffb58a4f524df3f7af09f8a92173790045c5
trevis <treviskevin77@gmail.com>
2024-06-15 21:14:38 +0200

    fixed result page

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 import React from 'react'
 
-const page = async () => {
+const page =  () => {
 	const idStr = localStorage.getItem("id");
 	const id = idStr ? parseInt(idStr) : 0;
-	const response = await fetch("/dataBikes.json");
-	const fileDataBike = await response.json();
-
-	const bike = fileDataBike.find((bike: any) => bike.id === id);
+	async function findBike() {
+		const response = await fetch("/dataBikes.json");
+		const fileDataBike = await response.json();
+		const bike = fileDataBike.find((bike: any) => bike.id === id);
+		return bike
+	}
+	let bike: any = findBike().then(res => bike = res)
 
 	return (
 		<div>Result page my friend l'id du bike est: {bike.id}
--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -1,25 +1,19 @@
 'use client'
 import { Button, divider } from "@nextui-org/react"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import React from "react"
 import Image from "next/image"
-import { useRouter } from "next/navigation"
 
 let mediaStream: MediaStream
 let mediaRecorder: MediaRecorder
 let chunks: Blob[] = []
 
 // @ts-ignore
-export default function AudioRecorder({ history, setHistory, setAudioUrl }) {
+export default function AudioRecorder({history, setHistory, setAudioUrl}) {
 	const [recording, setRecording] = useState(false)
-	const [isLoading, setIsLoading] = useState(false)
-	
-	const router = useRouter()
 
 	async function handleRecorderStop() {
-		setIsLoading(true)
-
-		const blob = new Blob(chunks, { type: "audio/wav" })
+		const blob = new Blob(chunks, {type: "audio/wav"})
 		const formData = new FormData()
 		formData.append("recording", blob)
 		let res = await fetch("/api/transcribe", {
@@ -27,26 +21,17 @@ export default function AudioRecorder({ history, setHistory, setAudioUrl }) {
 			body: formData
 		})
 		const json = await res.json()
-		const updateHistory = [...history, { role: "user", content: json }];
+		const updateHistory = [...history, {role: "user", content: json}];
 		setHistory(updateHistory)
 		res = await fetch("/api/think", {
 			method: "POST",
 			body: JSON.stringify(updateHistory)
 		})
-		const replyText:string = await res.json()
-		setHistory([...history, { role: "assistant", content: replyText }])
+		const replyText = await res.json()
+		setHistory([...history, {role: "assistant", content: replyText}])
 		chunks = []
-
-		if (replyText.startsWith("ID FOUND")) {
-			const id = replyText.split(":")[1]
-			localStorage.setItem("id", id);
-			router.push("/result")
-		}
-		const req = await fetch("/api/speech", { method: "POST", body: JSON.stringify(replyText) })
+		const req = await fetch("/api/speech", {method: "POST", body: JSON.stringify(replyText)})
 		const audio = await req.blob()
-		
-		setIsLoading(false)
-
 		setAudioUrl(URL.createObjectURL(audio))
 	}
 
@@ -57,18 +42,18 @@ export default function AudioRecorder({ history, setHistory, setAudioUrl }) {
 			mediaRecorder.stop()
 			return;
 		}
-		mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true })
+		mediaStream = await navigator.mediaDevices.getUserMedia({audio: true})
 		mediaRecorder = new MediaRecorder(mediaStream)
 		mediaRecorder.onstop = handleRecorderStop
 		mediaRecorder.ondataavailable = (event) => chunks.push(event.data)
 		mediaRecorder.start()
 	}
 
-	return (
-		<>
-			<Button isLoading={isLoading} onClick={toggleRecording} className="buttonPlay" size="lg" color={isLoading ? "warning" : (recording ? "danger" : "primary")}>
-				{!isLoading && <Image className="iconRecord" src={recording ? "/image/stopIcon.png" : "/image/playIcon.png"} alt="play icon" height={50} width={50} />}
-				{isLoading ? "Chargement" : (recording ? "Stop" : "Parler")}
+	return ( 
+		<>	
+			<Button onClick={toggleRecording} className="buttonPlay" size="lg" color={recording ? "danger" : "primary"}>
+							<Image className="iconRecord" src={recording ? "/image/stopIcon.png" : "/image/playIcon.png"} alt="play icon" height={50} width={50} />
+							{recording ? "Stop" : "Parler"}
 			</Button>
 		</>
 	)
