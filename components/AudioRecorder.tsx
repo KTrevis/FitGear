8c075b32fc170b657d2ac2643eb9082135968525
trevis <treviskevin77@gmail.com>
2024-06-15 21:31:51 +0200

    fix

--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -3,19 +3,20 @@ import { Button, divider } from "@nextui-org/react"
 import { useState } from "react"
 import React from "react"
 import Image from "next/image"
-import { useRouter } from "next/navigation"
 
 let mediaStream: MediaStream
 let mediaRecorder: MediaRecorder
 let chunks: Blob[] = []
 
 // @ts-ignore
-export default function AudioRecorder({history, setHistory, setAudioUrl}) {
+export default function AudioRecorder({ history, setHistory, setAudioUrl }) {
 	const [recording, setRecording] = useState(false)
-	const router = useRouter()
+	const [isLoading, setIsLoading] = useState(false)
 
 	async function handleRecorderStop() {
-		const blob = new Blob(chunks, {type: "audio/wav"})
+		setIsLoading(true)
+
+		const blob = new Blob(chunks, { type: "audio/wav" })
 		const formData = new FormData()
 		formData.append("recording", blob)
 		let res = await fetch("/api/transcribe", {
@@ -23,22 +24,22 @@ export default function AudioRecorder({history, setHistory, setAudioUrl}) {
 			body: formData
 		})
 		const json = await res.json()
-		const updateHistory = [...history, {role: "user", content: json}];
+		const updateHistory = [...history, { role: "user", content: json }];
 		setHistory(updateHistory)
 		res = await fetch("/api/think", {
 			method: "POST",
 			body: JSON.stringify(updateHistory)
 		})
-		const replyText: string = await res.json()
-		setHistory([...history, {role: "assistant", content: replyText}])
-		if (replyText.startsWith("ID FOUND")) {
-			const id = replyText.split(":")[1]
-			localStorage.setItem("id", id)
-			router.push("/result")
-		}
+
+		const replyText = await res.json()
+		setHistory([...history, { role: "assistant", content: replyText }])
 		chunks = []
-		const req = await fetch("/api/speech", {method: "POST", body: JSON.stringify(replyText)})
+
+		const req = await fetch("/api/speech", { method: "POST", body: JSON.stringify(replyText) })
 		const audio = await req.blob()
+		
+		setIsLoading(false)
+
 		setAudioUrl(URL.createObjectURL(audio))
 	}
 
@@ -49,18 +50,18 @@ export default function AudioRecorder({history, setHistory, setAudioUrl}) {
 			mediaRecorder.stop()
 			return;
 		}
-		mediaStream = await navigator.mediaDevices.getUserMedia({audio: true})
+		mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true })
 		mediaRecorder = new MediaRecorder(mediaStream)
 		mediaRecorder.onstop = handleRecorderStop
 		mediaRecorder.ondataavailable = (event) => chunks.push(event.data)
 		mediaRecorder.start()
 	}
 
-	return ( 
-		<>	
-			<Button onClick={toggleRecording} className="buttonPlay" size="lg" color={recording ? "danger" : "primary"}>
-							<Image className="iconRecord" src={recording ? "/image/stopIcon.png" : "/image/playIcon.png"} alt="play icon" height={50} width={50} />
-							{recording ? "Stop" : "Parler"}
+	return (
+		<>
+			<Button isLoading={isLoading} onClick={toggleRecording} className="buttonPlay" size="lg" color={isLoading ? "warning" : (recording ? "danger" : "primary")}>
+				{!isLoading && <Image className="iconRecord" src={recording ? "/image/stopIcon.png" : "/image/playIcon.png"} alt="play icon" height={50} width={50} />}
+				{isLoading ? "Chargement" : (recording ? "Stop" : "Parler")}
 			</Button>
 		</>
 	)
